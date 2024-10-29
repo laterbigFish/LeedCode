dd43d19a9724503869f2d51416dcb7eadf719127
azhu-pig <3347219161@qq.com>
2024-10-29 20:39:47 +0800

    LeedCode

--- untitled5/src/Solution.java
+++ untitled5/src/Solution.java
@@ -1,51 +1,89 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 class Solution {
-    public int[] tmp;
-    public int sortArray(int[] nums) {
-        tmp = new int[nums.length];
-        int ret = mergeSort(nums, 0, nums.length - 1);
-        return ret;
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,List<String>> hashMap = new HashMap<>();
+      for(String cur:strs){
+          char[] tmp = cur.toCharArray();
+          Arrays.sort(tmp);
+          String key = new String(tmp);
+          if(!hashMap.containsKey(key)){
+              hashMap.put(key,new ArrayList<>());
+          }
+          hashMap.get(key).add(cur);
+      }
+      //将哈希表中的元素放入ans
+        return  new ArrayList(hashMap.values());
     }
+    public  static boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer,Integer> hashMap = new HashMap<>();
 
-    public int mergeSort(int[] nums, int left, int right) {
-        if (left >= right)
-            return 0;
-        int ret = 0;
-        int mid = (left + right) / 2;
-        ret += mergeSort(nums, left, mid);
-        ret += mergeSort(nums, mid + 1, right);
-
-        int cur1 = left, cur2 = mid + 1, i = left;
-
-        while (cur1 <= mid) {
-
-            while (cur2 <= right && nums[cur2]*2L >= nums[cur1]*1L) cur2++;
-            if (cur2 > right)
-                break;
-            ret += right - cur2 + 1;
-            cur1++;
-        }
-        cur1 = left;
-        cur2 = mid + 1;
-        while (cur1 <= mid && cur2 <= right) {
-            if (nums[cur1] >= nums[cur2]) {
-                tmp[i++] = nums[cur1++];
-            } else {
-                tmp[i++] = nums[cur2++];
+        for(int i=0;i<nums.length;i++){
+            if(hashMap.containsKey(nums[i])){
+                if(Math.abs(i-hashMap.get(nums[i]))<=k) return true;
             }
+            hashMap.put(nums[i],i);
         }
-        while (cur1 <= mid)
-            tmp[i++] = nums[cur1++];
-        while (cur2 <= right)
-            tmp[i++] = nums[cur2++];
-        for (int k = left; k <= right; k++) {
-            nums[k] = tmp[k];
-        }
-        return ret;
+        return false;
     }
+    public  static  boolean CheckPermutation(String str1, String str2) {
+        if(str1.length()!=str2.length()) return false;
+
+        char[] cur1 =str1.toCharArray();
+        char[] cur2  = str2.toCharArray();
+        HashMap<Character,Integer> hashMap = new HashMap<>();
+        for(char cur:cur1){
+            hashMap.put(cur,hashMap.getOrDefault(cur,0)+1);
+        }
+        for(char cur:cur2){
+            if(hashMap.getOrDefault(cur,0)<=0) return false;
+            hashMap.put(cur,hashMap.getOrDefault(cur,0)-1);
+        }
+        return true;
+    }
+//    public int[] tmp;
+//    public int sortArray(int[] nums) {
+//        tmp = new int[nums.length];
+//        int ret = mergeSort(nums, 0, nums.length - 1);
+//        return ret;
+//    }
+//
+//    public int mergeSort(int[] nums, int left, int right) {
+//        if (left >= right)
+//            return 0;
+//        int ret = 0;
+//        int mid = (left + right) / 2;
+//        ret += mergeSort(nums, left, mid);
+//        ret += mergeSort(nums, mid + 1, right);
+//
+//        int cur1 = left, cur2 = mid + 1, i = left;
+//
+//        while (cur1 <= mid) {
+//
+//            while (cur2 <= right && nums[cur2]*2L >= nums[cur1]*1L) cur2++;
+//            if (cur2 > right)
+//                break;
+//            ret += right - cur2 + 1;
+//            cur1++;
+//        }
+//        cur1 = left;
+//        cur2 = mid + 1;
+//        while (cur1 <= mid && cur2 <= right) {
+//            if (nums[cur1] >= nums[cur2]) {
+//                tmp[i++] = nums[cur1++];
+//            } else {
+//                tmp[i++] = nums[cur2++];
+//            }
+//        }
+//        while (cur1 <= mid)
+//            tmp[i++] = nums[cur1++];
+//        while (cur2 <= right)
+//            tmp[i++] = nums[cur2++];
+//        for (int k = left; k <= right; k++) {
+//            nums[k] = tmp[k];
+//        }
+//        return ret;
+//    }
 //
 //    public int  mergeSort(int[] nums,int left,int right){
 //        int ret = 0;
@@ -212,9 +250,20 @@ class Solution {
 //
 //    }
 public static void main(String[] args) {
-    int[] array = {1,3,2,3,1};
-    Solution solution = new Solution();
-    System.out.println(solution.sortArray(array));
+//    int[] array = {1,3,2,3,1};
+//    Solution solution = new Solution();
+//    System.out.println(solution.sortArray(array));
+//    List<Integer> list = new ArrayList<>();
+//        String.toCharArray();
+//        String s = "afdsa";
+//        s.toCharArray()
+//    System.out.println(CheckPermutation("abc", "bc"));
+//    HashMap<Integer,Integer> hashMap = new HashMap<>();
+//    hashMap.get()
+    int[] array = {1,0,1,1};
+    System.out.println(containsNearbyDuplicate(array, 1));
     List<Integer> list = new ArrayList<>();
+    String ret = "afds";
+    Integer ret1 = 123;
 }
 }
