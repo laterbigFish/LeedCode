32fd7d8ad88973204780fbc47f224ce3f22c49da
Big_Fish <3347@qq.com>
2024-12-04 17:07:44 +0800

    src

--- untitled4/src/Main.java
+++ untitled4/src/Main.java
@@ -1,15 +1,99 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+// 定义一个抽象类
+abstract class Animal {
+    public final  static  int age = 10;
+    // 抽象方法，没有方法体  方法体可由子类填充
+    abstract void makeSound();
+//    abstract void func();
+    // 具体方法，有方法体
+    public  void eat() {
+        System.out.println("This animal eats food.");
+    }
+}
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+// 继承抽象类的子类
+class Dog extends Animal {
+    // 实现抽象方法
+    @Override
+    void makeSound() {
+        System.out.println("Bark");
+    }
+}
+
+  class Cat extends Dog {
+
+    @Override
+    void makeSound() {
+        System.out.println("Meow");
+    }
+}
+public class Main {
+    private final int[] dx = { 1, -1, 0, 0 };
+    private final int[] dy = { 0, 0, 1, -1 };
+    private boolean[][] vision;
+    private int ret;
+
+    public int uniquePathsIII(int[][] grid) {
+        vision = new boolean[grid.length][grid[0].length];
+
+        vision[0][0] = true;
+
+        dfs(grid,0,0);
+
+        return ret;
+    }
+
+    public void dfs(int[][] grid, int a, int b) {
+        if (grid[a][b] == 2) {
+            ret++;
+            return;
         }
+        for (int i = 0; i < 4; i++) {
+            int x = a + dx[i], y = b + dy[i];
+
+            if (x >= 0 && x < grid.length && y >= 0 && y < grid[0].length && !vision[x][y] && grid[x][y] == 0  && grid[x][y] != -1) {
+                vision[x][y] = true;
+                dfs(grid, x, y);
+                vision[x][y] = false;
+            }
+        }
+//    private final int[] dx = { 1, -1, 0, 0 };
+//    private final int[] dy = { 0, 0, 1, -1 };
+//    private boolean[][] vision;
+//    private int ret;
+//
+//    public int getMaximumGold(int[][] grid) {
+//        vision = new boolean[grid.length][grid[0].length];
+//
+//        for (int i = 0; i < grid.length; i++) {
+//            for (int j = 0; j < grid[0].length; j++) {
+//                if (grid[i][j] != 0)
+//                    dfs(grid, i, j, grid[i][j]);
+//            }
+//        }
+//        return ret;
+//    }
+//
+//    public void dfs(int[][] grid, int a, int b, int sum) {
+//        for (int i = 0; i < 4; i++) {
+//            int x = a = dx[i], y = dy[i];
+//            if (x >= 0 && y >= 0 && x < grid.length && y < grid[0].length && grid[x][y] != 0 && !vision[x][y]) {
+//                vision[a][b] = true;
+//                ret = Math.max(ret, sum + grid[a][b]);
+//                dfs(grid, x, y, sum + grid[a][b]);
+//                vision[a][b] = false;
+//            }
+//
+//        }
+//    }
+//    public static void main(String[] args) {
+        //        Animal dog = new Dog();
+//        dog.age = 10;
+//        Animal cat = new Cat();
+//
+//        dog.makeSound(); // 输出: Bark
+//        dog.eat();       // 输出: This animal eats food.
+//
+//        cat.makeSound(); // 输出: Meow
+//        cat.eat();       // 输出: This animal eats food.
     }
 }
